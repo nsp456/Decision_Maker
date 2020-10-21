38fcbc647ebdd1b4e2bfeacece1f9610f47d08a2
Nishant Pal <61820335+nsp456@users.noreply.github.com>
2020-10-21 22:55:39 +0530

    Add files via upload

---
+++ lib/flipCoin.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class FlipCoin extends StatefulWidget {
+  @override
+  _FlipCoinState createState() => _FlipCoinState();
+}
+
+class _FlipCoinState extends State<FlipCoin> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Flip A Coin'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Enter Head\'s value'),
+                validator: (v) {
+                  if (v.trim().isEmpty) return 'Please enter something';
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Enter Tail\'s name'),
+                validator: (v) {
+                  if (v.trim().isEmpty) return 'Please enter something';
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              FlatButton(
+                height: 50,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.blue)),
+                onPressed: () {},
+                child: Center(
+                  child: Text(
+                    'Flip Coin',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                color: Colors.lightGreen,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
