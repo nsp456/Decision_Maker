96120a291c7ab605e87cf69f3c4b72ac4f468399
Nishant Pal <61820335+nsp456@users.noreply.github.com>
2020-11-22 17:04:13 +0530

    Update auth.dart

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -1,17 +1,26 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-// class AuthService {
-//   final _auth = FirebaseAuth.instance;
+class AuthService {
+  final _firestore = FirebaseFirestore.instance;
+  dynamic data;
 
-//   // sign in anon
-//   Future create() async {
-//     try {
-//       AuthResult result = await _auth.signInAnonymously();
-//       FirebaseUser user = result.user;
-//       return user;
-//     } catch (e) {
-//       print(e.toString());
-//       return null;
-//     }
-//   }
-// }
+  addData() async {
+    _firestore.collection('user').add({"name": "Siddharth"});
+  }
+
+  fetchdata(String id) async {
+    CollectionReference collectionReference = _firestore.collection('user');
+    collectionReference.snapshots().listen((snapshot) {
+      for (int i = 0; i < snapshot.docs.length; i++) {
+        if (snapshot.docs[i].id == id) {
+          this.data = snapshot.docs[i].data();
+          print(data.toString() + '-----------------------------------');
+        }
+      }
+    });
+  }
+  // sign in anon
+
+}
