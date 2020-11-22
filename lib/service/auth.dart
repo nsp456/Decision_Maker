5fc93aa1087f8f74f5491da0cb61355c988253c2
Nishant Pal <61820335+nsp456@users.noreply.github.com>
2020-11-22 13:40:11 +0530

    Add files via upload

---
+++ lib/service/auth.dart
@@ -0,0 +1,17 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// class AuthService {
+//   final _auth = FirebaseAuth.instance;
+
+//   // sign in anon
+//   Future create() async {
+//     try {
+//       AuthResult result = await _auth.signInAnonymously();
+//       FirebaseUser user = result.user;
+//       return user;
+//     } catch (e) {
+//       print(e.toString());
+//       return null;
+//     }
+//   }
+// }
