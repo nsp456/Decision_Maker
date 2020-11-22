97fe04d21082f67391317f854f309a21e4813f59
Nishant Pal <61820335+nsp456@users.noreply.github.com>
2020-11-22 13:40:34 +0530

    Update formRandom.dart

--- lib/formRandom.dart
+++ lib/formRandom.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
 import 'Appbar.dart';
+import 'rr.dart';
 
 class MyForm extends StatefulWidget {
   @override
@@ -9,6 +11,20 @@ class MyForm extends StatefulWidget {
 
 class _MyFormState extends State<MyForm> {
   final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController;
+  static List<String> alternativesList = [null];
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,143 +32,162 @@ class _MyFormState extends State<MyForm> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
+        actions: <Widget>[
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.lightGreen,
+            child: IconButton(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                alternativesList.insert(0, null);
+                _getAlternatives();
+                setState(() {});
+              },
+            ),
+          ),
+        ],
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => MyHomePage()))),
         title: Text("Random Answer Generator"),
         flexibleSpace: App_bar(),
       ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Enter Your Choices',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new SingleChildScrollView(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(hintText: 'Choice 1'),
-                      validator: (v) {
-                        if (v.trim().isEmpty) return 'Please enter something';
-                        return null;
-                      },
-                    ),
+                  Text(
+                    'Enter Alternatives',
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
                   ),
+                  ..._getAlternatives(), // spread operator (...) provides a concise way to insert multiple elements into a collection.
                   SizedBox(
-                    width: 16,
+                    height: 40,
                   ),
-                  Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
+                  FlatButton(
+                    height: 50,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue)),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RandomResult()));
+                      }
+                    },
+                    child: Center(
+                      child: Text(
+                        'Generate Random Answer',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                    child: Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                    ),
+                    color: Colors.lightGreen,
                   ),
                 ],
               ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(hintText: 'Choice 2'),
-                      validator: (v) {
-                        if (v.trim().isEmpty) return 'Please enter something';
-                        return null;
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(hintText: 'Choice 3'),
-                      validator: (v) {
-                        if (v.trim().isEmpty) return 'Please enter something';
-                        return null;
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              FlatButton(
-                height: 50,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.blue)),
-                onPressed: () {},
-                child: Center(
-                  child: Text(
-                    'Generate Random Answer',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                color: Colors.lightGreen,
-              ),
-            ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  List<Widget> _getAlternatives() {
+    List<Widget> alternativesTextFields = [];
+    for (int i = 0; i < alternativesList.length; i++) {
+      alternativesTextFields.add(Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Row(
+          children: [
+            Expanded(child: AlternativeTextFields(i)),
+            SizedBox(
+              width: 16,
+            ),
+            _removeButton(i),
+          ],
+        ),
+      ));
+    }
+    return alternativesTextFields;
+  }
+
+  Widget _removeButton(int index) {
+    return InkWell(
+      onTap: () {
+        alternativesList.removeAt(index);
+        setState(() {});
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class AlternativeTextFields extends StatefulWidget {
+  final int index;
+  AlternativeTextFields(this.index);
+  @override
+  _AlternativeTextFieldsState createState() => _AlternativeTextFieldsState();
+}
+
+class _AlternativeTextFieldsState extends State<AlternativeTextFields> {
+  TextEditingController _nameController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _nameController.text = _MyFormState.alternativesList[widget.index] ?? '';
+    });
+
+    return TextFormField(
+      controller: _nameController,
+      onChanged: (v) => _MyFormState.alternativesList[widget.index] = v,
+      decoration: InputDecoration(hintText: 'Enter Alternative\'s name'),
+      validator: (v) {
+        if (v.trim().isEmpty) return 'Text field cannot be blank';
+        return null;
+      },
     );
   }
 }
